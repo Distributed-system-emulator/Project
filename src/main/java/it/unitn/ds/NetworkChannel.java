e8d244dc2f1cc7b9a5d2d49465500ec3a7015673
ErPreco <91291702+ErPreco@users.noreply.github.com>
2026-06-19 17:26:26 +0200

    formatted

--- src/main/java/it/unitn/ds/NetworkChannel.java
+++ src/main/java/it/unitn/ds/NetworkChannel.java
@@ -18,72 +18,71 @@ import scala.concurrent.duration.Duration;
  * pair.
  */
 public class NetworkChannel extends AbstractActor {
+  // Sent to self to trigger delivery of the head of the queue
+  private static class Deliver implements Serializable {
+  }
 
-    // Sent to self to trigger delivery of the head of the queue
-    private static class Deliver implements Serializable {
+  private final ActorRef destination;
+  private final int minLatency;
+  private final int maxLatency;
+  private final Random rnd;
+
+  // Queue of (message, originalSender) pairs waiting to be delivered
+  private final Queue<Object[]> queue = new LinkedList<>();
+  private boolean delivering = false;
+
+  private NetworkChannel(ActorRef destination, int minLatency, int maxLatency) {
+    this.destination = destination;
+    this.minLatency = minLatency;
+    this.maxLatency = maxLatency;
+    this.rnd = new Random();
+  }
+
+  public static Props props(ActorRef destination, int minLatency, int maxLatency) {
+    return Props.create(NetworkChannel.class,
+        () -> new NetworkChannel(destination, minLatency, maxLatency));
+  }
+
+  @Override
+  public Receive createReceive() {
+    return receiveBuilder()
+        .match(Deliver.class, msg -> onDeliver())
+        .matchAny(msg -> onEnqueue(msg, getSender()))
+        .build();
+  }
+
+  private void onEnqueue(Object msg, ActorRef sender) {
+    queue.add(new Object[] { msg, sender });
+    if (!delivering) {
+      scheduleNextDelivery();
     }
+  }
 
-    private final ActorRef destination;
-    private final int minLatency;
-    private final int maxLatency;
-    private final Random rnd;
-
-    // Queue of (message, originalSender) pairs waiting to be delivered
-    private final Queue<Object[]> queue = new LinkedList<>();
-    private boolean delivering = false;
-
-    private NetworkChannel(ActorRef destination, int minLatency, int maxLatency) {
-        this.destination = destination;
-        this.minLatency = minLatency;
-        this.maxLatency = maxLatency;
-        this.rnd = new Random();
+  private void onDeliver() {
+    Object[] entry = queue.poll();
+    if (entry == null) {
+      delivering = false;
+      return;
     }
+    Object msg = entry[0];
+    ActorRef originalSender = (ActorRef) entry[1];
+    destination.tell(msg, originalSender);
 
-    public static Props props(ActorRef destination, int minLatency, int maxLatency) {
-        return Props.create(NetworkChannel.class,
-                () -> new NetworkChannel(destination, minLatency, maxLatency));
+    if (!queue.isEmpty()) {
+      scheduleNextDelivery();
+    } else {
+      delivering = false;
     }
+  }
 
-    @Override
-    public Receive createReceive() {
-        return receiveBuilder()
-                .match(Deliver.class, msg -> onDeliver())
-                .matchAny(msg -> onEnqueue(msg, getSender()))
-                .build();
-    }
-
-    private void onEnqueue(Object msg, ActorRef sender) {
-        queue.add(new Object[] { msg, sender });
-        if (!delivering) {
-            scheduleNextDelivery();
-        }
-    }
-
-    private void onDeliver() {
-        Object[] entry = queue.poll();
-        if (entry == null) {
-            delivering = false;
-            return;
-        }
-        Object msg = entry[0];
-        ActorRef originalSender = (ActorRef) entry[1];
-        destination.tell(msg, originalSender);
-
-        if (!queue.isEmpty()) {
-            scheduleNextDelivery();
-        } else {
-            delivering = false;
-        }
-    }
-
-    private void scheduleNextDelivery() {
-        delivering = true;
-        int delay = minLatency + rnd.nextInt(maxLatency - minLatency);
-        getContext().system().scheduler().scheduleOnce(
-                Duration.create(delay, TimeUnit.MILLISECONDS),
-                getSelf(),
-                new Deliver(),
-                getContext().system().dispatcher(),
-                getSelf());
-    }
+  private void scheduleNextDelivery() {
+    delivering = true;
+    int delay = minLatency + rnd.nextInt(maxLatency - minLatency);
+    getContext().system().scheduler().scheduleOnce(
+        Duration.create(delay, TimeUnit.MILLISECONDS),
+        getSelf(),
+        new Deliver(),
+        getContext().system().dispatcher(),
+        getSelf());
+  }
 }
--- src/test/java/it/unitn/ds/base/WithCrashes.java
+++ src/test/java/it/unitn/ds/base/WithCrashes.java
@@ -26,85 +26,85 @@ import it.unitn.ds.AbstractReplica.Crash;
  * Check out the java doc and tests in `APICompliance.java`.
  */
 class WithCrashes {
+  @ParameterizedTest(name = "some non-coordinator replicas crash, client writes, waits and reads => coordinator {0}, nodes {1}")
+  @CsvSource({
+      "7",
+      "22",
+  })
+  void nonCoordinatorsCrashClientWritesWaitsReads(int n_nodes) throws InterruptedException {
+    final int COORDINATOR_ID = 0;
+    final TestsSystemWrapper sys = TestsCommons
+        .createTestSystem("nonCoordinatorsCrashClientWritesWaitsReads_" + COORDINATOR_ID, n_nodes,
+            COORDINATOR_ID);
+    TestKit probe = new TestKit(sys.system);
+    int targetReplicaID = n_nodes - 1;
+    ActorRef client = sys.system.actorOf(
+        Client.propsWithListener(sys.client_read_timeout, sys.client_write_timeout,
+            Optional.ofNullable(sys.actors.get(targetReplicaID)), probe.getRef()),
+        "client");
 
-	@ParameterizedTest(name = "some non-coordinator replicas crash, client writes, waits and reads => coordinator {0}, nodes {1}")
-	@CsvSource({
-			"7",
-			"22",
-	})
-	void nonCoordinatorsCrashClientWritesWaitsReads(int n_nodes) throws InterruptedException {
-		final int COORDINATOR_ID = 0;
-		final TestsSystemWrapper sys = TestsCommons
-				.createTestSystem("nonCoordinatorsCrashClientWritesWaitsReads_" + COORDINATOR_ID, n_nodes,
-						COORDINATOR_ID);
-		TestKit probe = new TestKit(sys.system);
-		int targetReplicaID = n_nodes - 1;
-		ActorRef client = sys.system.actorOf(
-				Client.propsWithListener(sys.client_read_timeout, sys.client_write_timeout,
-						Optional.ofNullable(sys.actors.get(targetReplicaID)), probe.getRef()),
-				"client");
+    for (int i = 0; i < n_nodes / 2 - 2; i++) {
+      sys.actors.get(i + 1).tell(new Crash(Crash.Type.Now, 0), Actor.noSender());
+    }
 
-		for (int i = 0; i < n_nodes / 2 - 2; i++) {
-			sys.actors.get(i + 1).tell(new Crash(Crash.Type.Now, 0), Actor.noSender());
-		}
+    client.tell(new AbstractClient.WriteRequest(TestsCommons.TEST_INDEX, TestsCommons.TEST_VALUE),
+        Actor.noSender());
+    WriteResult wr = (WriteResult) probe.fishForMessage(
+        Duration.ofMillis(TestsCommons.getMaxUpdateDelay(sys)),
+        "WriteResult",
+        msg -> msg instanceof WriteResult);
+    assertEquals(
+        new WriteResult(true, TestsCommons.TEST_INDEX, TestsCommons.TEST_VALUE, targetReplicaID), wr);
 
-		client.tell(new AbstractClient.WriteRequest(TestsCommons.TEST_INDEX, TestsCommons.TEST_VALUE),
-				Actor.noSender());
-		WriteResult wr = (WriteResult) probe.fishForMessage(
-				Duration.ofMillis(TestsCommons.getMaxUpdateDelay(sys)),
-				"WriteResult",
-				msg -> msg instanceof WriteResult);
-		assertEquals(
-				new WriteResult(true, TestsCommons.TEST_INDEX, TestsCommons.TEST_VALUE, targetReplicaID), wr);
+    Thread.sleep(TestsCommons.getMaxUpdateDelay(sys));
 
-		Thread.sleep(TestsCommons.getMaxUpdateDelay(sys));
+    client.tell(new AbstractClient.ReadRequest(TestsCommons.TEST_INDEX), Actor.noSender());
+    ReadResult rr = (ReadResult) probe.fishForMessage(
+        Duration.ofMillis(sys.client_read_timeout),
+        "ReadResult",
+        msg -> msg instanceof ReadResult);
+    assertEquals(
+        new ReadResult(true, TestsCommons.TEST_INDEX, TestsCommons.TEST_VALUE, targetReplicaID), rr);
 
-		client.tell(new AbstractClient.ReadRequest(TestsCommons.TEST_INDEX), Actor.noSender());
-		ReadResult rr = (ReadResult) probe.fishForMessage(
-				Duration.ofMillis(sys.client_read_timeout),
-				"ReadResult",
-				msg -> msg instanceof ReadResult);
-		assertEquals(
-				new ReadResult(true, TestsCommons.TEST_INDEX, TestsCommons.TEST_VALUE, targetReplicaID), rr);
+    sys.system.terminate();
+  }
 
-		sys.system.terminate();
-	}
+  @ParameterizedTest(name = "coordinator and other 2 replicas crash, client writes, waits and reads => coordinator {0}, nodes {1}")
+  @CsvSource({
+      "1,7",
+      "0,22",
+  })
+  void coordinatorCrashClientWritesWaitsReads(int coordinator, int n_nodes) throws InterruptedException {
+    final TestsSystemWrapper sys = TestsCommons
+        .createTestSystem("coordinatorCrashClientWritesWaitsReads_" + coordinator, n_nodes, coordinator);
+    TestKit probe = new TestKit(sys.system);
+    int targetReplicaID = n_nodes - 1;
+    ActorRef client = sys.system.actorOf(
+        Client.propsWithListener(sys.client_read_timeout, sys.client_write_timeout,
+            Optional.ofNullable(sys.actors.get(targetReplicaID)), probe.getRef()),
+        "client");
 
-	@ParameterizedTest(name = "coordinator and other 2 replicas crash, client writes, waits and reads => coordinator {0}, nodes {1}")
-	@CsvSource({
-			"1,7",
-			"0,22",
-	})
-	void coordinatorCrashClientWritesWaitsReads(int coordinator, int n_nodes) throws InterruptedException {
-		final TestsSystemWrapper sys = TestsCommons.createTestSystem("coordinatorCrashClientWritesWaitsReads_" + coordinator, n_nodes, coordinator);
-		TestKit probe = new TestKit(sys.system);
-		int targetReplicaID = n_nodes - 1;
-		ActorRef client = sys.system.actorOf(
-				Client.propsWithListener(sys.client_read_timeout, sys.client_write_timeout,
-						Optional.ofNullable(sys.actors.get(targetReplicaID)), probe.getRef()),
-				"client");
+    for (int i = 0; i < 3; i++) {
+      sys.actors.get(i).tell(new Crash(Crash.Type.Now, 0), Actor.noSender());
+    }
 
-		for (int i = 0; i < 3; i++) {
-			sys.actors.get(i).tell(new Crash(Crash.Type.Now, 0), Actor.noSender());
-		}
+    client.tell(new AbstractClient.WriteRequest(TestsCommons.TEST_INDEX, TestsCommons.TEST_VALUE),
+        Actor.noSender());
+    WriteResult wr = (WriteResult) probe.fishForMessage(
+        Duration.ofMillis(TestsCommons.getElectionMaxDelay(sys) + TestsCommons.getMaxUpdateDelay(sys)),
+        "WriteResult",
+        msg -> msg instanceof WriteResult);
+    assertEquals(
+        new WriteResult(true, TestsCommons.TEST_INDEX, TestsCommons.TEST_VALUE, targetReplicaID), wr);
 
-		client.tell(new AbstractClient.WriteRequest(TestsCommons.TEST_INDEX, TestsCommons.TEST_VALUE),
-				Actor.noSender());
-		WriteResult wr = (WriteResult) probe.fishForMessage(
-				Duration.ofMillis(TestsCommons.getElectionMaxDelay(sys) + TestsCommons.getMaxUpdateDelay(sys)),
-				"WriteResult",
-				msg -> msg instanceof WriteResult);
-		assertEquals(
-				new WriteResult(true, TestsCommons.TEST_INDEX, TestsCommons.TEST_VALUE, targetReplicaID), wr);
+    client.tell(new AbstractClient.ReadRequest(TestsCommons.TEST_INDEX), Actor.noSender());
+    ReadResult rr = (ReadResult) probe.fishForMessage(
+        Duration.ofMillis(TestsCommons.getLatencyPlusEpsilon(sys)),
+        "ReadResult",
+        msg -> msg instanceof ReadResult);
+    assertEquals(
+        new ReadResult(true, TestsCommons.TEST_INDEX, TestsCommons.TEST_VALUE, targetReplicaID), rr);
 
-		client.tell(new AbstractClient.ReadRequest(TestsCommons.TEST_INDEX), Actor.noSender());
-		ReadResult rr = (ReadResult) probe.fishForMessage(
-				Duration.ofMillis(TestsCommons.getLatencyPlusEpsilon(sys)),
-				"ReadResult",
-				msg -> msg instanceof ReadResult);
-		assertEquals(
-				new ReadResult(true, TestsCommons.TEST_INDEX, TestsCommons.TEST_VALUE, targetReplicaID), rr);
-
-		sys.system.terminate();
-	}
+    sys.system.terminate();
+  }
 }
